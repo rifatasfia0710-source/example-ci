88b94f0b339a9d500cd44ef4fca6af0efcf36566
ratri <rifatasfia0710@gmail.com>
2026-01-03 13:57:35 +0600

    commitfinal

--- function.py
+++ function.py
@@ -7,5 +7,8 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
-def convert_fahrenheit_to_celsius(fahrenheit):
-    return multiply(subtract(fahrenheit, 32), 9 / 5) # <-- Fix this in step 7
+# def convert_fahrenheit_to_celsius(fahrenheit):
+#     return multiply(subtract(fahrenheit, 32), 9 / 5) # <-- Fix this in step 7
+def convert_fahrenheit_to_celsius(f):
+    return (f - 32) * 5 / 9
+
